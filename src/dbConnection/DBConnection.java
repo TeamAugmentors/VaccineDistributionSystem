cd0fdc3103734fd8177049290a563924bcd0dcc9
AhmedSaadTanim <astdragneel@yahoo.com>
2021-09-26 14:41:32 +0600

    final 1.0

--- src/dbConnection/DBConnection.java
+++ src/dbConnection/DBConnection.java
@@ -34,17 +34,17 @@ public class DBConnection {
             String password = null;
             
 //            <============MD. ATIKUR RAHMAN============>
-//            String password = "123456";
-//            String user = "Atiqur";
-//            String url = "jdbc:sqlserver://localhost\\SQLExpress:1433;databaseName=VaccinationCenter";
+//            password = "123456";
+//            user = "Atiqur";
+//            url = "jdbc:sqlserver://localhost\\SQLExpress:1433;databaseName=VaccinationCenter";
 //            <--------------- Ahmed Saad Tanim ------------->
-//            String password = "123456";
-//            String user = "AST";
-//            String url = "jdbc:sqlserver://AST-PC\\SQLEXPRESS:1433;databaseName=VaccinationCenter";
+//            password = "123456";
+//            user = "AST";
+//            url = "jdbc:sqlserver://AST-PC\\SQLEXPRESS:1433;databaseName=VaccinationCenter";
 //            <--------------- Sanjid Islam Chowdhury ----------->
-//            String password = "123456";
-//            String user = "san2";
-//            String url = "jdbc:sqlserver://lassassins-blad\\SQLEXPRESS:1433;databaseName=VaccinationCenter";
+//            password = "123456";
+//            user = "san2";
+//            url = "jdbc:sqlserver://lassassins-blad\\SQLEXPRESS:1433;databaseName=VaccinationCenter";
 
             connection = DriverManager.getConnection(url, user, password);
             clientId = connection.toString().substring(35, connection.toString().length());
